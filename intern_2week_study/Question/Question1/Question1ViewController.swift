772003f2d25da73586982203cc03678b79f10e6b
leo-hdk <leo.syuukatu@gmail.com>
2020-08-11 19:47:25 +0900

    課題１の機能の追加

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -4,9 +4,36 @@ final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var addTextButton: UIButton!
+    @IBOutlet weak var clearTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //画面表示時にTextViewをクリア
+        textView.text = ""
+        label.text = ""
+    }
+    
+    @IBAction func tapButton(_ sender: Any) {
+        var a: String
+        var b: String
+        
+        if textField.text == "" {
+            label.text = "文字を入力してください"
+            
+        } else {
+            a = textField.text!
+            b = textView.text
+            //Buttonを押すとTextFieldの文字をTextViewに表示
+            //textView.text = textField.text
+            textView.text = b + "\n" + a
+            
+            label.text = ""
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        textView.text = ""
     }
 }
